dc69db67db32f7f61147a6f18e8ed4d6724a5d94
Gustavo Castro <gstvcastroc@gmail.com>
2022-09-28 21:53:16 -0300

    (chore): implemented routes and first components

--- src/Frontend/SISGEP.Front/src/app/app-routing.module.ts
+++ src/Frontend/SISGEP.Front/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MasterComponent } from './pages/master/master.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { QuestionaryComponent } from './pages/questionary/questionary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MasterComponent,
+    children: [
+    { path: 'home', component: HomeComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'questionary', component: QuestionaryComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/Frontend/SISGEP.Front/src/app/app.module.ts
+++ src/Frontend/SISGEP.Front/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { MasterComponent } from './pages/master/master.component';
+import { QuestionaryComponent } from './pages/questionary/questionary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    MasterComponent,
+    QuestionaryComponent
   ],
   imports: [
     BrowserModule,
