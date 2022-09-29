c1266663ef927360d63260ad828864993b34f352
Gustavo Castro <gstvcastroc@gmail.com>
2022-09-29 19:51:18 -0300

    (chore): adjusts in projects and questionary comp

--- src/Frontend/SISGEP.Front/src/app/pages/questionary/questionary.component.ts
+++ src/Frontend/SISGEP.Front/src/app/pages/questionary/questionary.component.ts
@@ -12,4 +12,11 @@ export class QuestionaryComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  questionarios: any = [
+    { id : 1, questionario: 'Cadastro de Alpha', projeto: 'Projeto Alpha', datainc : '2022-09-29 00:00:00' },
+    { id : 2, questionario: 'Cadastro de Beta', projeto: 'Projeto Beta', datainc : '2022-09-29 00:00:00' },
+    { id : 3, questionario: 'Cadastro de Gama', projeto: 'Projeto Alpha', datainc : '2022-09-29 00:00:00' },
+    { id : 4, questionario: 'Cadastro de Andromeda', projeto: 'Projeto Beta', datainc : '2022-09-29 00:00:00' },
+    { id : 5, questionario: 'Cadastro de Orion', projeto: 'Projeto Alpha', datainc : '2022-09-29 00:00:00' },
+    { id : 6, questionario: 'Cadastro de Morpheus', projeto: 'Projeto Beta', datainc : '2022-09-29 00:00:00' }];
 }
