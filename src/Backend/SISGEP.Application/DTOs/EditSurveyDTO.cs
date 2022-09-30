5d81fd4078e06bf0eb0fc67af4df302009cc5f18
Alonso de Oliveira <alonsoboj@gmail.com>
2022-09-29 22:30:21 -0300

    (chore): edit survey dto.

---
+++ src/Backend/SISGEP.Application/DTOs/EditSurveyDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SISGEP.Application.DTOs
+{
+    public class EditSurveyDTO
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; }
+
+        public string Structure { get; set; } = string.Empty;
+
+        public Guid ProjectId { get; set; }
+    }
+}
