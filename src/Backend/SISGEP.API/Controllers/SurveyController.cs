bb8768bc88510d2b799b71c0974181ed357d1c0c
Alonso de Oliveira <alonsoboj@gmail.com>
2022-09-29 22:26:56 -0300

    (chore): adds a survey controller class.

---
+++ src/Backend/SISGEP.API/Controllers/SurveyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SISGEP.Application.Contracts;
+using SISGEP.Application.DTOs;
+using System.Threading.Tasks;
+
+namespace SISGEP.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SurveyController : ControllerBase
+    {
+        private readonly ISurveyService _surveyService;
+
+        public SurveyController(ISurveyService surveyService)
+        {
+            _surveyService = surveyService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] EditSurveyDTO dto)
+        {
+            await _surveyService.CreateSurveyAsync(dto);
+
+            return Ok();
+        }
+    }
+}
