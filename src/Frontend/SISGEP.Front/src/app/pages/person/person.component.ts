fc575b19ac049bb086a647c2b71acb45120a8957
Gustavo Castro <gstvcastroc@gmail.com>
2022-11-30 19:49:19 -0300

    (chore): adjust at persons http get

--- src/Frontend/SISGEP.Front/src/app/pages/person/person.component.ts
+++ src/Frontend/SISGEP.Front/src/app/pages/person/person.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import { Observable, throwError } from 'rxjs';
-import { catchError, map, retry } from 'rxjs/operators';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { map } from 'rxjs/operators';
 
 import { Person } from 'src/app/interface/person-interface';
 import { CreatePersonComponent } from './create/create-person/create-person.component';
@@ -19,24 +18,22 @@ export class PersonComponent implements OnInit {
 
   constructor(private _httpClient: HttpClient, private _modalService: NgbModal) { }
 
+  personList : Person[] = [];
+
   ngOnInit(): void {
+
+    this.personList = this.loadPerson();
   }
 
   _httpRequestUrl = 'https://localhost:5001/api/';
 
-  loadPerson() : Observable<Person[]> {
+  loadPerson() : Person[] {
 
-    /*let personList: Person[] = [
-      { id: 1, name: 'Renato Mateus da Mota', docIdentify: '962.107.576-93' },
-      { id: 2, name: 'Aparecida Bruna Maria Nascimento', docIdentify: '781.210.576-80' },
-      { id: 3, name: 'Helena Elaine Castro', docIdentify: '398.649.646-74' },
-      { id: 4, name: 'Jorge Marcos Carlos Souza', docIdentify: '204.206.126-37' },
-      { id: 5, name: 'Eloá Benedita Melo', docIdentify: '802.743.926-40' },
-      { id: 6, name: 'Marcelo Thales Lucca de Paula', docIdentify: '494.035.466-03' },
-      { id: 7, name: 'Marcelo Thiago Paulo Galvão', docIdentify: '183.547.516-78' }
-    ];*/
+    let person : Person[] = [];
 
-    return this._httpClient.get<Person[]>(this._httpRequestUrl + 'Person');
+    this._httpClient.get<Person[]>(`${this._httpRequestUrl}Person`).pipe(map(response => <Person[]>response)).subscribe((data : Person[]) => person = data);
+
+    return person;
   }
 
   newPerson() : void{
